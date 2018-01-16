982eb45bb60b1eac7a0685e83736bc5c4dc15f5c
Daniil Matveev <dnmtvf@gmail.com>
2018-01-16 11:45:50 +0300

    Fix App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import basset from './basset.jpg';
 import './App.sss';
 
-const test = (props) => {
-  return (
-    <div className="App">
-      <h1>Hi!</h1>
-      <h2>Edit <span className="App-Path">src/App.jsx</span> to start making your app</h2>
-    </div>
-  );
-};
+const App = () => (
+  <div className="App">
+    <h1>Hi!</h1>
+    <h2>Edit <span className="App-Path">src/App.jsx</span> to start making your app</h2>
+  </div>
+);
 
-export default test;
+export default App;
